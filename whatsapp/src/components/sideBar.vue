5d867bfcac437339992b38f022c04349f483c408
gokturkdogan <gokturk.dogan@hotmail.com>
2022-07-16 23:09:31 +0300

    assets folder editted css file imported

--- whatsapp/src/components/sideBar.vue
+++ whatsapp/src/components/sideBar.vue
@@ -2,7 +2,7 @@
     <div class="sideBar">
         <div class="sideBar__header">
             <div class="sideBar__profilePhoto">
-                <img src="../assets/user1.jpg" alt="">
+                <img src="../assets/images/user1.jpg" alt="">
 
             </div>
             <div class="sideBar__headerIcons">
@@ -29,7 +29,7 @@
         <div class="sideBar__body">
             <div class="sideBar__item">
                 <div class="sideBar__itemImage">
-                    <img src="../assets/user2.jpg" alt="">
+                    <img src="../assets/images/user2.jpg" alt="">
                 </div>
                 <div class="sideBar__sender">
                     <div class="sideBar__senderInfo">
@@ -43,7 +43,7 @@
             </div>
             <div class="sideBar__item">
                 <div class="sideBar__itemImage">
-                    <img src="../assets/user2.jpg" alt="">
+                    <img src="../assets/images/user2.jpg" alt="">
                 </div>
                 <div class="sideBar__sender">
                     <div class="sideBar__senderInfo">
@@ -57,7 +57,7 @@
             </div>
             <div class="sideBar__item">
                 <div class="sideBar__itemImage">
-                    <img src="../assets/user2.jpg" alt="">
+                    <img src="../assets/images/user2.jpg" alt="">
                 </div>
                 <div class="sideBar__sender">
                     <div class="sideBar__senderInfo">
@@ -71,7 +71,7 @@
             </div>
             <div class="sideBar__item">
                 <div class="sideBar__itemImage">
-                    <img src="../assets/user2.jpg" alt="">
+                    <img src="../assets/images/user2.jpg" alt="">
                 </div>
                 <div class="sideBar__sender">
                     <div class="sideBar__senderInfo">
@@ -85,7 +85,7 @@
             </div>
             <div class="sideBar__item">
                 <div class="sideBar__itemImage">
-                    <img src="../assets/user2.jpg" alt="">
+                    <img src="../assets/images/user2.jpg" alt="">
                 </div>
                 <div class="sideBar__sender">
                     <div class="sideBar__senderInfo">
@@ -99,7 +99,7 @@
             </div>
             <div class="sideBar__item">
                 <div class="sideBar__itemImage">
-                    <img src="../assets/user2.jpg" alt="">
+                    <img src="../assets/images/user2.jpg" alt="">
                 </div>
                 <div class="sideBar__sender">
                     <div class="sideBar__senderInfo">
@@ -113,7 +113,7 @@
             </div>
             <div class="sideBar__item">
                 <div class="sideBar__itemImage">
-                    <img src="../assets/user2.jpg" alt="">
+                    <img src="../assets/images/user2.jpg" alt="">
                 </div>
                 <div class="sideBar__sender">
                     <div class="sideBar__senderInfo">
@@ -127,7 +127,7 @@
             </div>
             <div class="sideBar__item">
                 <div class="sideBar__itemImage">
-                    <img src="../assets/user2.jpg" alt="">
+                    <img src="../assets/images/user2.jpg" alt="">
                 </div>
                 <div class="sideBar__sender">
                     <div class="sideBar__senderInfo">
@@ -141,7 +141,7 @@
             </div>
             <div class="sideBar__item">
                 <div class="sideBar__itemImage">
-                    <img src="../assets/user2.jpg" alt="">
+                    <img src="../assets/images/user2.jpg" alt="">
                 </div>
                 <div class="sideBar__sender">
                     <div class="sideBar__senderInfo">
@@ -153,7 +153,7 @@
                     </div>
                 </div>
             </div>
-            
+
         </div>
     </div>
 </template>
@@ -168,175 +168,5 @@ export default {
 </script>
 
 <style>
-.sideBar {
-    width: 700px;
-}
-
-.sideBar__header {
-    display: flex;
-    justify-content: space-between;
-    background-color: #202C33;
-    padding: 10px 20px;
-}
-
-.sideBar__profilePhoto {
-
-}
-
-.sideBar__profilePhoto img {
-    width: 65px;
-    border-radius: 55px;
-    width: 55px;
-    cursor: pointer;
-}
-
-.sideBar__headerIcons {
-    display: flex;
-    justify-content: space-between;
-    width: 200px;
-    padding-right: 35px;
-}
-
-.sideBar__headerIcon {
-    margin-top: 10px;
-    font-size: 30px;
-    color: #AEBAC1;
-    cursor: pointer;
-}
-
-.sideBar__search {
-    display: flex;
-    justify-content: space-between;
-    background-color: #111B21;
-    border-bottom: 1px solid #202C33;
-    padding: 10px 15px;
-}
-
-.sideBar__searchArea {
-    padding: 10px 20px;
-    border-radius: 10px;
-    background-color: #202C33;
-    width: 100%;
-    display: flex;
-}
-
-.sideBar__searchArea input {
-    background: none;
-    border: none;
-    resize: none;
-    outline: none;
-    width: 100%;
-    margin-left: 30px;
-    color: #AEBAC1;
-    font-weight: 100;
-    font-size: 20px;
-}
-
-::placeholder {
-    color: #AEBAC1;
-    font-size: 20px;
-    font-weight: 100;
-}
-
-.sideBar__searchIcon {
-    font-size: 20px;
-    color: #AEBAC1;
-    cursor: pointer;
-}
-
-.sideBar_searchFilter {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    padding: 0 5px;
-    margin-left: 20px;
-    cursor: pointer;
-}
-
-.sideBar__archive {
-    cursor: pointer;
-    display: flex;
-    justify-content: flex-start;
-    padding: 15px 35px;
-    background-color: #111B21;
-    border-bottom: 1px solid #202C33;
-}
-
-.sideBar__archiveIcons {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-}
-
-.sideBar__archiveIcon {
-    font-size: 20px;
-    color: #00A884;
-}
-
-.sideBar__archive span {
-    color: white;
-    font-size: 25px;
-    margin-left: 50px;
-}
-
-.sideBar__body {
-    height: 700px;
-    Overflow-y: scroll;
-}
-
-.sideBar__item {
-    display: flex;
-    background-color: #111B21;
-    padding: 10px 15px;
-    cursor: pointer;
-}
-
-.sideBar__item :hover{
-    background-color: #202C33;
-}
-
-.sideBar__itemImage {
-}
-
-.sideBar__itemImage img {
-    border-radius: 55px;
-    width: 65px;
-}
-
-.sideBar__sender {
-    display: flex;
-    width: 100%;
-    justify-content: space-between;
-    margin-left: 20px;
-    border-bottom: 1px solid #202C33;
-    
-}
-
-.sideBar__senderInfo{
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-}
-
-.sideBar__senderName {
-    color: white;
-    font-size: 25px;
-}
-
-.sideBar__senderMessage {
-    color: #AEBAC1;
-    font-size: 20px;
-}
-
-.sideBar__senderMessageTime{
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-}
-
-.sideBar__senderMessageTime span{
-    color: #AEBAC1;
-}
-
-
+@import '../assets/css/sideBar.css';
 </style>
