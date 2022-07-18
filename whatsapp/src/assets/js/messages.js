cad21bd60301ef6246e86bde49b6e64d9f872a21
gokturkdogan <gokturk.dogan@hotmail.com>
2022-07-19 01:41:15 +0300

    message file editted

--- whatsapp/src/assets/js/messages.js
+++ whatsapp/src/assets/js/messages.js
@@ -1,48 +1,58 @@
 const messagesIndexes = [
     {
-        "user": "Steve Jobs",
+        "user": "Stefan Salvatore",
         "image": "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80",
         "messages": [
             {
                 "time": "10:15",
-                "content": "Jobs, quando sai o novo Iphone?",
+                "content": "Hi there Darius, are you free?",
                 "send": true
             },
             {
                 "time": "10:15",
-                "content": "Em setembro de 2019",
+                "content": "Hey Stefan, what is it?",
+                "send": false
+            },
+            {
+                "time": "10:15",
+                "content": "We are going to play basketball tomorrow, would you like to coin?",
+                "send": true
+            },
+            {
+                "time": "10:15",
+                "content": "That seems great, add me to team!",
                 "send": false
             },
         ]
     },
     {
-        "user": "Bill Gates",
+        "user": "Elena Gilbert",
         "image": "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80",
         "messages": [
             {
                 "time": "10:15",
-                "content": "Eu queria umas dicas suas para o próximo Windows!",
+                "content": "Hey Darius are you coming concert tomorrow night?",
                 "send": false
             },
             {
                 "time": "10:15",
-                "content": "Não sei se consigo... Estou meio enrolado.",
+                "content": "oh, sorry Elena I am going to play basketball with Stefan.",
                 "send": true
             },
         ]
     },
     {
-        "user": "Jeff Bezos",
+        "user": "Matt Donovan",
         "image": "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80",
         "messages": [
             {
                 "time": "10:15",
-                "content": "O que você achou do novo recurso da AWS?!",
+                "content": "Darius can you send me photo of your homework?",
                 "send": false
             },
             {
                 "time": "10:15",
-                "content": "?!",
+                "content": "I am on the way right now. Let me call you back.",
                 "send": false
             },
         ]
