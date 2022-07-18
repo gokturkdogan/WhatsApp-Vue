c9a08fda1652443a498c39734f2f3fbcaa6c0dfd
gokturkdogan <gokturk.dogan@hotmail.com>
2022-07-18 22:19:27 +0300

    message data js file created

---
+++ whatsapp/src/assets/js/messages.js
@@ -0,0 +1,51 @@
+const messagesIndexes = [
+    {
+        "user": "Steve Jobs",
+        "image": "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80",
+        "messages": [
+            {
+                "time": "10:15",
+                "content": "Jobs, quando sai o novo Iphone?",
+                "send": true
+            },
+            {
+                "time": "10:15",
+                "content": "Em setembro de 2019",
+                "send": false
+            },
+        ]
+    },
+    {
+        "user": "Bill Gates",
+        "image": "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80",
+        "messages": [
+            {
+                "time": "10:15",
+                "content": "Eu queria umas dicas suas para o próximo Windows!",
+                "send": false
+            },
+            {
+                "time": "10:15",
+                "content": "Não sei se consigo... Estou meio enrolado.",
+                "send": true
+            },
+        ]
+    },
+    {
+        "user": "Jeff Bezos",
+        "image": "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80",
+        "messages": [
+            {
+                "time": "10:15",
+                "content": "O que você achou do novo recurso da AWS?!",
+                "send": false
+            },
+            {
+                "time": "10:15",
+                "content": "?!",
+                "send": false
+            },
+        ]
+    }
+];
+export default messagesIndexes;
